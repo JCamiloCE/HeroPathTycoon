106a3e52322d205e99b0946a55a9c3e7bb9205ff
Juan Cortes <jcamilocoes@gmail.com>
2025-03-07 18:29:25 -0500

    JCC - change sprite and add more files into the gitignore

--- Assets/Scripts/Buildings/BuildingData.cs
+++ Assets/Scripts/Buildings/BuildingData.cs
@@ -8,7 +8,6 @@ namespace Buildings
     {
         [SerializeField] private EBuildingType _buildingType;
         [SerializeField] private Sprite _initialSprite;
-        //private float _timeToEvolveHero; Soon
 
         public EBuildingType GetBuildingType => _buildingType;
         public Sprite GetBuildingInitialSprite => _initialSprite;
