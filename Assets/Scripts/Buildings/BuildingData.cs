2f09e2e682a316159f6413ed5ed22975881d06c5
Juan Cortes <jcamilocoes@gmail.com>
2025-05-15 13:35:22 -0500

    JCC - new behavior to change the banner of the building and fill the scriptable object

--- Assets/Scripts/Buildings/BuildingData.cs
+++ Assets/Scripts/Buildings/BuildingData.cs
@@ -9,12 +9,14 @@ namespace HeroPath.Scripts.Buildings
     {
         [SerializeField] private EBuildingType _buildingType;
         [SerializeField] private Sprite _initialSprite;
+        [SerializeField] private Sprite _bannerSprite;
         [SerializeField] private float _timeToProcess;
         [SerializeField] private int _goldPerProcess;
         [SerializeField] private int _price;
 
         public EBuildingType GetBuildingType => _buildingType;
         public Sprite GetBuildingInitialSprite => _initialSprite;
+        public Sprite GetBannerBuildingSprite => _bannerSprite;
         public float GetBuildingTimeToProcess => _timeToProcess;
         public int GetBuildingGoldPerProcess => _goldPerProcess;
         public int GetBuildingPrice => _price;
